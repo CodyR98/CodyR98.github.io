486b49db42e30e38a8aaf4384ee963c66cd49f1a
Cody Roberts <S3976990@student.rmit.edu.au>
2022-09-09 19:07:38 +1000

    large changes
    added javascript
    
    fixed format

---
+++ header.js
@@ -0,0 +1,21 @@
+document.write('\
+<div>\
+	<nav>\
+	<div id="menu-outer">\
+	<div class="table">\
+		<ul id="horizontal-list">\
+			<li><a href="index.html">HOME</a></li>\
+			<li><a href="IdealJob.html">IDEAL JOB</a></li>\
+			<li><a href="InterestinIT.html">IT Interest</a></li>\
+			<li><a href="Project.html">PROJECT</a></li>\
+			<li><a href="PersonalProfile.html">Personal Profile</a></li>\
+		</ul>\
+	</nav>\
+	</div>\
+		<div class="header-left">\
+		<img src="/Graphics/RMIT.png">\
+	</div>\
+		<div class="header-right">\
+		<img src="/Graphics/RMIT.png">\
+	</div>\
+');
